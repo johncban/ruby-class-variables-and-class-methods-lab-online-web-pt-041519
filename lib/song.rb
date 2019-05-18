74087aebfb4864a75460ad6c52fa63d72c9b79ec
johncban <johncban@gmail.com>
2019-05-18 19:21:26 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song
+  attr_accessor :name, :artist, :genre
+
+  @@count = 0
+  @@genres = Array.new
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @@genres << genre
+    @@count +=1
+    @@artists << artist
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.genres
+    # Source: https://apidock.com/ruby/Array/uniq
+    @@genres.uniq
+  end
+
+  def self.artists
+    # Source: https://apidock.com/ruby/Array/uniq
+    @@artists.uniq
+  end
+
+  def self.genre_count
+    genre_count ={}
+
+    @@genres.each do |genre|
+      if genre_count[genre]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+
+=begin
+  @@genres.each_with_object(genre_count){
+    |genre| genre_count[genre] += 1
+  }
+  end
+=end
+
+  def self.artist_count
+
+  end
+
+end
